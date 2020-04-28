aa7ab9a1e5e7da9dd0ccf6ab936283d54541e0ab
kiskovi97 <kiskovi97@gmail.com>
2020-04-28 11:38:06 +0200

    Navigation between fragments and activities with Menu

--- app/src/main/java/hu/bme/sch/parkett/parkettapplication/framework/activities/DanceActivity.kt
+++ app/src/main/java/hu/bme/sch/parkett/parkettapplication/framework/activities/DanceActivity.kt
@@ -1,7 +1,10 @@
 package hu.bme.sch.parkett.parkettapplication.framework.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import hu.bme.sch.parkett.parkettapplication.R
 import hu.bme.sch.parkett.parkettapplication.framework.fragments.DanceEditFragment
 import hu.bme.sch.parkett.parkettapplication.framework.fragments.DanceReadFragment
@@ -9,14 +12,46 @@ import kotlinx.android.synthetic.main.activity_dance.*
 
 class DanceActivity : AppCompatActivity() {
 
+    var selectedId: Int? = null
+    var edit: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dance)
         val id = intent.getIntExtra(DANCE_ID, -1)
         dance_activity_debug.text = id.toString()
-
+        selectedId = id
         supportFragmentManager.beginTransaction().replace(R.id.fragment_dance_read, DanceReadFragment.newInstance(id)).commit()
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_dance_edit, DanceEditFragment.newInstance(id)).commit()
+        edit = false
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.readview_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.deleteIcon -> {
+
+            }
+            R.id.editIcon -> {
+                if (selectedId != null) {
+                    if (edit) {
+                        supportFragmentManager.beginTransaction().replace(R.id.fragment_dance_read, DanceReadFragment.newInstance(selectedId!!)).commit()
+                        item.icon = ContextCompat.getDrawable(this, R.drawable.ic_edit_white_24dp)
+                        edit = false
+                    } else {
+                        supportFragmentManager.beginTransaction().replace(R.id.fragment_dance_read, DanceEditFragment.newInstance(selectedId!!)).commit()
+                        item.icon = ContextCompat.getDrawable(this, R.drawable.ic_remove_red_eye_whit_24dp)
+                        edit = true
+                    }
+
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 

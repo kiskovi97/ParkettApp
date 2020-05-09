f3e5c4bf04844c6bcd9ade816d241169e3a2c597
kiskovi97 <kiskovi97@gmail.com>
2020-05-09 10:26:08 +0200

    Save Test For DataBase

---
+++ app/src/test/java/hu/bme/sch/parkett/parkettapplication/unit/DanceInteractorTest.kt
@@ -0,0 +1,52 @@
+package hu.bme.sch.parkett.parkettapplication.unit
+
+import android.content.Context
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.orm.SugarContext
+import com.orm.SugarRecord
+import hu.bme.sch.parkett.parkettapplication.database.DataBase
+import hu.bme.sch.parkett.parkettapplication.interactor.DanceInteractor
+import hu.bme.sch.parkett.parkettapplication.model.Dance
+import hu.bme.sch.parkett.parkettapplication.network.DancesApi
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+class DanceInteractorTest {
+
+
+    @Test
+    fun addDance_HappyPath() {
+        //Arrange
+        val mockedDataBase = mock(DataBase::class.java)
+        val interactor = DanceInteractor(mock(DancesApi::class.java), mockedDataBase)
+        val addedDance = Dance(-1, "MockDance",null,null)
+
+        val context = mock(Context::class.java)
+        SugarContext.init(context)
+
+        //Act
+        interactor.addDance(addedDance)
+
+        //Assert
+        verify(mockedDataBase).save(addedDance)
+    }
+
+    @Test
+    fun saveDance_HappyPath() {
+        //Arrange
+        val mockedDataBase = mock(DataBase::class.java)
+        val interactor = DanceInteractor(mock(DancesApi::class.java), mockedDataBase)
+        val addedDance = Dance(-1, "MockDance",null,null)
+
+        val context = mock(Context::class.java)
+        SugarContext.init(context)
+
+        //Act
+        interactor.saveDance(addedDance)
+
+        //Assert
+        verify(mockedDataBase).save(addedDance)
+    }
+}
